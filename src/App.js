a3ffadf36a83531496344a29774aabcb74c19a1a
Jaqueline <jaquelineotazu25@gmail.com>
2023-11-01 20:58:24 -0600

    V5. Mi formulario ELIMINA Y ACTUALIZA

--- src/App.js
+++ src/App.js
@@ -1,20 +1,75 @@
 //import logo from './logo.svg';
-//import './App.css';
+import { collection, getDocs, query, doc, deleteDoc, where, onSnapshot } from 'firebase/firestore';
+import './App.css';
+import { useEffect, useState } from 'react';
+import Appform from './components/AppForm';
+//import C01componente from './components/C01componente';
+//import Variable from './components/C04variable';
+//import MatrizOperaciones from './components/P7matrizOperaciones';
+import { db } from "./firebase/firebase";
 
-
-import AppForm from "./components/AppForm";
 
 function App() {
-  return (
-    <div style={{background:"yellow", 
-    width:"350px", 
-    padding:"10px"}}>
-      <AppForm/>
-      <i class="large material-icons">child_care</i>
 
-      <p class="text-dark">1. juan manuel 23 masculino ----  x  - A</p>
-      <p class="text-dark">2. rosa maria 28 Femenino  ------ x  - A</p>
-      <p class="text-dark">3. bryan david 30 masculino ----  x  - A</p>
+   ///// READ - LECTURA - fnRead //////
+   const [docBD, setDocBD] = useState([]);
+   
+   const fnRead = () => {
+    try {
+      const xColeccionConQuery = query(collection(db, "persona"));
+      //const xColeccionConQuery = query(collection(db, "persona", where("nombre", "!=", "")))
+      const unsubscribe = onSnapshot(xColeccionConQuery, (xDatosBD) => {
+        const xDoc = [];
+        xDatosBD.forEach( (doc) => {
+          //xDoc.push(doc.data().nombre);                       ///Datos como "texto" en array
+          //xDoc.push(doc.id);                                  ///Datos "ID" como "texto" en array
+          //xDoc.push(doc.data());                              ///Datos como "Objeto"
+          //xDoc.push({id: doc.id});                            ///Datos "ID" como "objeto" con indice "id"  
+          xDoc.push({id: doc.id, ...doc.data()});
+          //console(id: doc.id, ...doc.data())
+          //console.log({id: docV.id, ...docV.data()});
+        });
+        //console.log("Resultado...: ", xDoc.join(", "));       ///Comentar sino bucle infinito
+        setDocBD(xDoc);
+        //console.log(docsBD);                                  ///Error lectura debe ser afuera
+      });
+
+    } catch (error) {
+      console.error(error);
+    }
+  }
+  //fnRead();
+  useEffect( () => {
+    fnRead();
+  }, []);
+  
+
+   ///// DELETE - ELIMINAR - fnDelete /////
+   const [idActual, setIdActual] = useState("");
+   const fnDelete = async (xId) =>{
+    //console.log(xId);
+    if (window.confirm("Confirme para eliminar")){
+      await deleteDoc(doc(db, 'persona', xId));
+      console.log("Se elimino... "+xId);
+    } 
+    //fnRead();     //No es necesario, fue cambiado por otra fn en useEffect
+   }
+
+  return (
+    <div style={{background:"darkviolet", width:"350px", padding:"10px", textAlign:"center"}}>
+      <h1>JAQUELINE App.js</h1>
+      <Appform {...{idActual, setIdActual, fnRead}}></Appform>
+      <i class="large material-icons">child_care</i>
+      {
+        docBD.map((p) =>
+          <p key={p.id}>
+            No. 1 {p.nombre} ....
+            <span onClick={() => fnDelete(p.id)}> x </span>
+            ...
+            <span onClick={() => setIdActual(p.id)}> A </span>
+          </p>
+        )
+      }
     </div>
   );
 }
--- src/firebase/firebase.js
+++ src/firebase/firebase.js
@@ -1,29 +1,27 @@
 // Import the functions you need from the SDKs you need
 import { initializeApp } from "firebase/app";
 import { getAnalytics } from "firebase/analytics";
-
 import { getAuth } from "firebase/auth";
 import { getStorage, ref, uploadBytes } from "firebase/storage";
-import { getFirestore, addDoc, collection, getDoc, getDocs,query, where, setDoc, deleteDoc, updateDoc } from "firebase/firestore";  
+import { getFirestore, addDoc, collection, getDoc, getDocs, query, where, setDoc, deleteDoc, updateDoc } from "firebase/firestore";
 // TODO: Add SDKs for Firebase products that you want to use
 // https://firebase.google.com/docs/web/setup#available-libraries
 
 // Your web app's Firebase configuration
 // For Firebase JS SDK v7.20.0 and later, measurementId is optional
 const firebaseConfig = {
-  apiKey: "AIzaSyCfH4D_QrJ9sjZZ-43gg4B635HNo6iF4pQ",
-  authDomain: "proy-react-app02.firebaseapp.com",
-  projectId: "proy-react-app02",
-  storageBucket: "proy-react-app02.appspot.com",
-  messagingSenderId: "811109286834",
-  appId: "1:811109286834:web:5b58832eef2728a9cd5aab",
-  measurementId: "G-GWSR5V7S3S"
+  apiKey: "AIzaSyA5Q9Mluav9De8PyTxt7u7PGK6BvUvZun0",
+  authDomain: "proy-react-app04.firebaseapp.com",
+  projectId: "proy-react-app04",
+  storageBucket: "proy-react-app04.appspot.com",
+  messagingSenderId: "900309723146",
+  appId: "1:900309723146:web:9da580898ad9bcff2a17f3",
+  measurementId: "G-EP7N3D1W0Z"
 };
 
 // Initialize Firebase
 const app = initializeApp(firebaseConfig);
 const analytics = getAnalytics(app);
-
 const auth = getAuth(app);
 export const db = getFirestore(app);
-const storage = getStorage(app)
+const storage = getStorage(app);
